a338eeb23a0d30a0a3b9270d909fe8adf04ffdd0
Riley Paul <rileypaul96@gmail.com>
2025-09-04 06:47:28 -0600

    copy lots of files over

---
+++ src/db/schema.ts
@@ -0,0 +1,63 @@
+import {
+  integer,
+  sqliteTable,
+  text,
+  type AnySQLiteColumn,
+} from "drizzle-orm/sqlite-core";
+
+const id = text("id")
+  .primaryKey()
+  .$defaultFn(() => crypto.randomUUID());
+
+const userId = text()
+  .notNull()
+  .references(() => User.id, { onDelete: "cascade" });
+
+const timeStamps = {
+  createdAt: text()
+    .notNull()
+    .$defaultFn(() => new Date().toISOString()),
+  updatedAt: text()
+    .notNull()
+    .$defaultFn(() => new Date().toISOString())
+    .$onUpdateFn(() => new Date().toISOString()),
+};
+
+export const User = sqliteTable("user", {
+  id,
+  email: text().notNull().unique(),
+  name: text().notNull(),
+  avatarUrl: text(),
+
+  googleId: text().unique(),
+  githubId: integer().unique(),
+  githubUsername: text().unique(),
+  ...timeStamps,
+});
+
+export const UserSession = sqliteTable("userSession", {
+  id,
+  userId,
+  expiresAt: integer({ mode: "timestamp_ms" }).notNull(),
+  ...timeStamps,
+});
+
+export const Drawing = sqliteTable("drawing", {
+  id,
+  userId,
+  name: text().notNull().default("Untitled"),
+  parentFolderId: text().references(() => Folder.id, { onDelete: "set null" }),
+  fileSize: integer().default(0),
+  savedAt: text(),
+  ...timeStamps,
+});
+
+export const Folder = sqliteTable("folder", {
+  id,
+  userId,
+  name: text().notNull().default("New Folder"),
+  parentFolderId: text().references((): AnySQLiteColumn => Folder.id, {
+    onDelete: "set null",
+  }),
+  ...timeStamps,
+});
---
+++ src/pages/contact.ts
@@ -0,0 +1,12 @@
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async () => {
+  // get form data
+  // check that the form data is valid
+  // lookup which project the form data is for
+  // send email to project owner
+  // send email to user
+  // return either redirect or json response
+
+  return new Response();
+};
