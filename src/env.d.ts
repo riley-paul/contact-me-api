12a4f9e07615a2940d7bf493a04b584315e5997c
Riley Paul <rileypaul96@gmail.com>
2025-09-04 06:47:18 -0600

    copy lots of files over

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,11 @@
-type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+type ENV = {} & import("./envs").Environment;
+
+type Runtime = import("@astrojs/cloudflare").Runtime<ENV>;
 
 declare namespace App {
-	interface Locals extends Runtime {}
+  interface Locals extends Runtime {
+    session: import("./lib/types").UserSessionInfo | null;
+    user: import("./lib/types").UserSelect | null;
+  }
 }
