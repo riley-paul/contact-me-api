e92886219f6a1935970148ea624e35b165338c9e
Riley Paul <rileypaul96@gmail.com>
2025-09-08 17:12:15 -0600

    add search bar

--- src/app/routes/projects.tsx
+++ src/app/routes/projects.tsx
@@ -1,20 +1,23 @@
-import {
-  Button,
-  Heading,
-  IconButton,
-  ScrollArea,
-  Separator,
-  TextField,
-  Tooltip,
-} from "@radix-ui/themes";
+import { Button, Heading, Separator } from "@radix-ui/themes";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { PlusIcon, SearchIcon } from "lucide-react";
+import { PlusIcon } from "lucide-react";
+import SearchBar from "../components/search-bar";
+import { z } from "astro/zod";
+import { useDebounceCallback } from "usehooks-ts";
 
 export const Route = createFileRoute("/projects")({
   component: RouteComponent,
+  validateSearch: z.object({ search: z.string().optional() }),
 });
 
 function RouteComponent() {
+  const { search } = Route.useSearch();
+  const navigate = Route.useNavigate();
+
+  const debouncedSetSearch = useDebounceCallback((search) =>
+    navigate({ search: { search } }),
+  );
+
   return (
     <div className="flex flex-1 flex-col">
       <header className="flex h-14 shrink-0 items-center justify-between px-6">
@@ -22,9 +25,7 @@ function RouteComponent() {
           Projects
         </Heading>
         <section className="flex items-center gap-2">
-          <IconButton variant="soft">
-            <SearchIcon className="size-4" />
-          </IconButton>
+          <SearchBar search={search} setSearch={debouncedSetSearch} />
           <Button variant="solid">
             <PlusIcon className="size-4" />
             Add Project
