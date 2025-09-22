2493397189e07c4ca7f8ed90700d291183376afe
Riley Paul <rileypaul96@gmail.com>
2025-09-22 13:45:27 -0600

    revamp design

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,16 +1,22 @@
-import type { ProjectSelect } from '@/lib/types';
-import { Card } from '@radix-ui/themes';
-import React from 'react';
+import type { ProjectSelect } from "@/lib/types";
+import { Card, Text } from "@radix-ui/themes";
+import React from "react";
 
-type Props = { project: ProjectSelect }
+type Props = { project: ProjectSelect };
 
 const ProjectCard: React.FC<Props> = ({ project }) => {
-  return <a>
-    <Card>
-      {project.name}
-    </Card>
-  </a>
-  ;
+  return (
+    <a href={`/projects/${project.id}`}>
+      <Card size="2">
+        <Text as="div" size="2" weight="bold">
+          {project.name}
+        </Text>
+        <Text as="div" size="2" color="gray">
+          {project.description}
+        </Text>
+      </Card>
+    </a>
+  );
 };
 
 export default ProjectCard;
