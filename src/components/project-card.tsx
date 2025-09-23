46ed777020f3f7adde1d0b30a2bc89eb80269c4b
Riley Paul <rileypaul96@gmail.com>
2025-09-23 05:30:55 -0600

    add more pages

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,21 +1,29 @@
 import type { ProjectSelect } from "@/lib/types";
 import { Card, Text } from "@radix-ui/themes";
+import { KeyIcon } from "lucide-react";
 import React from "react";
 
 type Props = { project: ProjectSelect };
 
 const ProjectCard: React.FC<Props> = ({ project }) => {
   return (
-    <a href={`/projects/${project.id}`}>
-      <Card size="2">
-        <Text as="div" size="2" weight="bold">
-          {project.name}
-        </Text>
-        <Text as="div" size="2" color="gray">
-          {project.description}
-        </Text>
-      </Card>
-    </a>
+    <Card size="2" asChild>
+      <a href={`/projects/${project.id}`}>
+        <article className="flex items-center gap-3">
+          <div className="bg-grayA-3 flex size-8 items-center justify-center rounded-full">
+            <KeyIcon className="size-4" />
+          </div>
+          <section>
+            <Text as="div" size="2" weight="bold">
+              {project.name}
+            </Text>
+            <Text as="div" size="2" color="gray">
+              {project.description}
+            </Text>
+          </section>
+        </article>
+      </a>
+    </Card>
   );
 };
 
