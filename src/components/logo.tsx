a88dd630774009d007866bef237b1e89560ea53b
Riley Paul <rileypaul96@gmail.com>
2025-10-05 16:45:36 -0600

    clean up packages

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,8 +5,10 @@ import React from "react";
 const Logo: React.FC = () => {
   return (
     <a href="/">
-      <span className="flex items-center gap-1.5">
-        <SendIcon className="text-accent-10 size-6 mt-0.5" />
+      <span className="flex items-center gap-2">
+        <div className="bg-accent-3 flex size-9 items-center justify-center rounded-full">
+          <SendIcon className="text-accent-10 mt-0.5 mr-0.5 size-5" />
+        </div>
         <Heading size="3" weight="bold">
           Contactulator
         </Heading>
