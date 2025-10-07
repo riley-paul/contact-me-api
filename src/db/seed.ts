a2ee09809c8eb8efc5ccdf222f927949ab0e619e
Riley Paul <rileypaul96@gmail.com>
2025-10-06 20:21:51 -0600

    refine seed script

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,4 +1,5 @@
-import { Project, User } from "./schema";
+import { Message, Project, User } from "./schema";
+import { faker } from "@faker-js/faker";
 
 import env from "@/envs-runtime";
 import { createDb } from ".";
@@ -23,18 +24,37 @@ export default async function seed() {
   const projects = await db
     .insert(Project)
     .values(
-      Array.from({ length: 30 }).map((_, idx) => ({
+      Array.from({ length: 7 }).map((_, idx) => ({
         userId,
-        identifier: `my-project-${idx + 1}`,
-        name: `My Project ${idx + 1}`,
-        description: "This is my project",
-        repoUrl: "",
-        liveUrl: "",
+        identifier: faker.lorem.slug(faker.number.int({ min: 1, max: 3 })),
+        name: faker.lorem
+          .sentence(faker.number.int({ min: 1, max: 3 }))
+          .replaceAll(".", ""),
+        description: faker.lorem.sentences(
+          faker.number.int({ min: 1, max: 3 }),
+        ),
+        repoUrl: faker.internet.url(),
+        liveUrl: faker.internet.url(),
       })),
     )
     .returning();
 
   console.log(`✅ Seeded ${projects.length} projects`);
+
+  for (const project of projects) {
+    const length = faker.number.int({ min: 0, max: 70 });
+    if (length > 0) {
+      await db.insert(Message).values(
+        Array.from({ length }).map((_, idx) => ({
+          projectId: project.id,
+          name: faker.person.fullName(),
+          email: faker.internet.email(),
+          content: faker.lorem.paragraphs(),
+        })),
+      );
+    }
+    console.log(`✅ Seeded ${length} messages for project ${project.id}`);
+  }
 }
 
 seed().catch((e) => {
