d3523e6d6ef28bf6b5bf051612ac4a6e19cc679a
Riley Paul <rileypaul96@gmail.com>
2025-10-07 09:40:13 -0600

    add project list

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,11 +1,10 @@
-import { IconButton, Separator } from "@radix-ui/themes";
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { Separator } from "@radix-ui/themes";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import { DraftingCompassIcon } from "lucide-react";
 import NavBar from "../components/nav-bar";
 
 const RootLayout = () => (
-  <main className="flex min-h-screen overflow-hidden">
+  <main className="flex h-screen overflow-hidden">
     <NavBar />
     <Separator orientation="vertical" size="4" />
     <div className="flex-1">
--- src/app/routes/projects.tsx
+++ src/app/routes/projects.tsx
@@ -1,9 +1,32 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Separator, TextField } from "@radix-ui/themes";
+import { createFileRoute } from "@tanstack/react-router";
+import { SearchIcon } from "lucide-react";
 
-export const Route = createFileRoute('/projects')({
+export const Route = createFileRoute("/projects")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/projects"!</div>
+  return (
+    <div className="flex h-screen">
+      <aside className="w-[250px]">
+        <header className="h-14">
+          <TextField.Root
+            variant="soft"
+            size="3"
+            className="h-full bg-transparent"
+            style={{ borderRadius: 0 }}
+            placeholder="Search projects..."
+          >
+            <TextField.Slot side="left">
+              <SearchIcon className="text-accent-10 size-4" />
+            </TextField.Slot>
+          </TextField.Root>
+        </header>
+        <Separator size="4" />
+        <div>Projects List</div>
+      </aside>
+      <Separator orientation="vertical" size="4" />
+    </div>
+  );
 }
