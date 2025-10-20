d750d0cb8b386ddaac62ff6983fcb33a3c7846ec
Riley Paul <rileypaul96@gmail.com>
2025-10-20 07:37:32 -0600

    add user menu to header

--- src/app/components/user-menu.tsx
+++ src/app/components/user-menu.tsx
@@ -2,10 +2,10 @@ import { LogOutIcon } from "lucide-react";
 import { Avatar, DropdownMenu, Text } from "@radix-ui/themes";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { qUser } from "@/app/queries";
+import type { UserSelect } from "@/lib/types";
+import RadixProvider from "./radix-provider";
 
-export const UserMenu: React.FC = () => {
-  const { data: user } = useSuspenseQuery(qUser);
-
+export const UserMenu: React.FC<{ user: UserSelect }> = ({ user }) => {
   const fallback = user.name
     .split(" ")
     .map((n) => n[0])
@@ -13,44 +13,47 @@ export const UserMenu: React.FC = () => {
     .toUpperCase();
 
   return (
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger>
-        <Avatar
-          src={user.avatarUrl ?? ""}
-          radius="full"
-          size="2"
-          fallback={fallback}
-        />
-      </DropdownMenu.Trigger>
-
-      <DropdownMenu.Content
-        side="right"
-        align="end"
-        onCloseAutoFocus={(e) => e.preventDefault()}
-      >
-        <header className="flex items-center gap-2 p-2">
+    <RadixProvider asChild>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
           <Avatar
             src={user.avatarUrl ?? ""}
             radius="full"
-            size="5"
+            size="2"
             fallback={fallback}
           />
-          <div className="grid">
-            <Text weight="bold">{user.name}</Text>
-            <Text size="2" color="gray">
-              {user.email}
-            </Text>
-          </div>
-        </header>
-        <DropdownMenu.Separator />
-        <DropdownMenu.Item asChild color="amber">
-          <a href="/logout">
-            <LogOutIcon className="size-4" />
-            <span>Log out</span>
-          </a>
-        </DropdownMenu.Item>
-      </DropdownMenu.Content>
-    </DropdownMenu.Root>
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content
+          side="bottom"
+          align="end"
+          sideOffset={8}
+          onCloseAutoFocus={(e) => e.preventDefault()}
+        >
+          <header className="flex items-center gap-2 p-2">
+            <Avatar
+              src={user.avatarUrl ?? ""}
+              radius="full"
+              size="4"
+              fallback={fallback}
+            />
+            <div className="grid">
+              <Text weight="bold">{user.name}</Text>
+              <Text size="2" color="gray">
+                {user.email}
+              </Text>
+            </div>
+          </header>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Item asChild color="amber">
+            <a href="/logout">
+              <LogOutIcon className="size-4" />
+              <span>Log out</span>
+            </a>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </RadixProvider>
   );
 };
 
