b70bfb2dee6836917eb0dc2a5cb2bfa9e47cf2ed
Riley Paul <rileypaul96@gmail.com>
2025-10-20 10:13:31 -0600

    get messages searching working

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,17 +1,39 @@
-import { TextField } from "@radix-ui/themes";
-import { SearchIcon } from "lucide-react";
+import { Button, IconButton, TextField } from "@radix-ui/themes";
+import { SearchIcon, XIcon } from "lucide-react";
 import React from "react";
 
-type Props = { search: string | null };
+type Props = { search: string | null; url: URL };
 
-const SearchForm: React.FC<Props> = ({ search }) => {
+const SearchForm: React.FC<Props> = ({ search, url }) => {
   return (
-    <form>
-      <TextField.Root name="search">
+    <form className="flex gap-2">
+      <TextField.Root
+        variant="soft"
+        name="search"
+        defaultValue={search ?? ""}
+        placeholder="Search..."
+      >
         <TextField.Slot side="left">
-          <SearchIcon className="size-4" />
+          <SearchIcon className="text-accent-10 size-4" />
         </TextField.Slot>
+        {search && (
+          <TextField.Slot side="right">
+            <IconButton
+              color="red"
+              variant="ghost"
+              radius="full"
+              size="1"
+              asChild
+            >
+              <a href={url.pathname}>
+                <XIcon className="text-red-10 size-4" />
+              </a>
+            </IconButton>
+          </TextField.Slot>
+        )}
       </TextField.Root>
+      <input type="submit" hidden />
+      <Button value="surface">Go</Button>
     </form>
   );
 };
