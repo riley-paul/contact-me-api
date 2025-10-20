9ca460e6427f17fbf7f64ad54aa9805e613f0425
Riley Paul <rileypaul96@gmail.com>
2025-10-20 16:41:04 -0600

    add project page

--- src/app/routes/projects.$projectId.tsx
+++ src/app/routes/projects.$projectId.tsx
@@ -1,9 +1,73 @@
-import { createFileRoute } from '@tanstack/react-router'
+import MessageTable from "@/components/message-table";
+import ProjectEmailAdd from "@/components/project-email-add";
+import ProjectEmailTable from "@/components/project-email-table";
+import ProjectTags from "@/components/project-tags";
+import SearchForm from "@/components/search-form";
+import { Heading, Text } from "@radix-ui/themes";
+import { createFileRoute } from "@tanstack/react-router";
+import { actions } from "astro:actions";
+import { z } from "astro:schema";
+import React from "react";
+import { useDebounceCallback } from "usehooks-ts";
 
-export const Route = createFileRoute('/projects/$projectId')({
+export const Route = createFileRoute("/projects/$projectId")({
   component: RouteComponent,
-})
+  loaderDeps: ({ search: { search, page } }) => ({ search, page }),
+  loader: async ({ params: { projectId }, deps: { search, page } }) => {
+    const [project, messages] = await Promise.all([
+      actions.projects.getOne.orThrow({ projectId }),
+      actions.messages.getAll.orThrow({ projectId, search, page }),
+    ]);
+    return { project, messages };
+  },
+  validateSearch: z.object({
+    search: z.string().optional(),
+    page: z.number().optional(),
+  }),
+});
 
 function RouteComponent() {
-  return <div>Hello "/projects/$projectId"!</div>
+  const { project, messages } = Route.useLoaderData();
+  const { search } = Route.useSearch();
+  const navigate = Route.useNavigate();
+
+  const setSearch = useDebounceCallback((search: string | undefined) => {
+    navigate({ to: ".", search: (old) => ({ ...old, search }) });
+  });
+
+  const setPage = (page: number) => {
+    navigate({ search: (old) => ({ ...old, page }) });
+  };
+
+  return (
+    <React.Fragment>
+      <Heading size="8">{project.name}</Heading>
+      <ProjectTags project={project} />
+      <Text size="2" color="gray">
+        {project.description}
+      </Text>
+      <section className="grid gap-4">
+        <header className="flex items-center justify-between">
+          <Heading as="h2">Emails</Heading>
+          <ProjectEmailAdd projectId={project.id} />
+        </header>
+        <ProjectEmailTable projectEmails={project.emails} />
+        <Text color="gray" size="2">
+          Messages sent to this project will be forwarded to all of the emails
+          above.
+        </Text>
+      </section>
+      <section className="grid gap-4">
+        <header className="flex items-center justify-between">
+          <Heading>Messages</Heading>
+          <SearchForm search={search} setSearch={setSearch} />
+        </header>
+        <MessageTable
+          messages={messages.messages}
+          pagination={messages.pagination}
+          setPage={setPage}
+        />
+      </section>
+    </React.Fragment>
+  );
 }
